61ad53ae0b72554efc51802dc7a830f10c981a6c
Yuriy Netesov <yuriy.netesov@gmail.com>
2016-04-08 08:25:19 +0200

    first almost working version. territory scoring is still sucks

--- go.hs
+++ go.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Data.Maybe
 import Data.Either
+import qualified Data.Text
 
 
 data GoStone = BlackStone | WhiteStone
@@ -162,7 +163,7 @@ put_stone player pos go =
           if (is_free_position go pos)
           then if (length (stone_group_liberties new_go new_stone) > 0)
                then Right new_go
-               else Left "stone group got no liberties"
+               else Left $ "stone group got no liberties with new stone at " ++ show pos
           else Left $  "place is not free: " ++ (show pos)
       else Left ("not turn of that player " ++ show player)
 
@@ -240,8 +241,36 @@ score :: GoPlayer -> GoState -> Integer
 score player go = toInteger $ length $ estimated_player_area player go
 
 
-make_a_turn :: GoPlayer -> GoState -> GoState
-make_a_turn = undefined
+data GoGameContinuation = ContinueGame GoState
+                        | StopGame GoState
+                          deriving (Show, Eq)
+
+make_a_turn :: GoPlayer -> GoState -> IO GoGameContinuation
+make_a_turn player go = do
+    putStrLn "make your turn: [p]ut stone at <y> <x>, p[a]ss, [d]rawn"
+    choice <- getLine
+    if length choice == 0
+    then do
+      putStrLn "invalid input"
+      make_a_turn player go
+    else do
+      case head choice of
+        'p' -> do
+           let parse_position input = map (\s -> (read (Data.Text.unpack s)) :: Integer)
+                                      (filter (/=Data.Text.empty) (Data.Text.split (==' ')
+                                                                       (Data.Text.pack (tail input))))
+           let (row: col: []) =  parse_position choice
+           case put_stone player (row, col) go of
+                Left reason -> do
+                    putStrLn $ "invalid turn : " ++ reason
+                    putStrLn "try more"
+                    make_a_turn player go
+                Right next_go -> return $ ContinueGame $ next_go
+        'a' -> return $ ContinueGame $ GoState (gostate_board go) (gostate_stones go) (opposite_player player)
+        'd' -> return $ StopGame go
+        other -> do
+           putStrLn "invalid input. try more"
+           make_a_turn player go
 
 
 game_loop :: GoPlayer -> GoState -> IO ()
@@ -250,9 +279,13 @@ game_loop player go = do
     BlackPlayer -> putStrLn "black player turn"
     WhitePlayer -> putStrLn "white player turn"
   putStrLn $ show_game go
-  putStrLn "make your turn: [p]ut stone, p[a]ss, [d]rawn"
-  choice <- getLine
-  putStrLn choice
+  next_go <- make_a_turn player go
+  case next_go of
+    ContinueGame next_go -> game_loop (opposite_player player) next_go
+    StopGame stop_go ->
+        do
+          putStrLn "game ended"
+    
     
 
 main :: IO ()
